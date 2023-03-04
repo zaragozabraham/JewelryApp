4bb214ffc37755805096c7acfeabd173e5a1078c
zaragozabraham <abrahamezg@gmail.com>
2023-03-03 18:41:35 -0600

    Add new libraries Add Native Navigation Add react-native-heroicons Add react-native-svg Add pods @ ios

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+import { SparklesIcon } from "react-native-heroicons/solid";
+
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+        <Text>Home Screen xd</Text>
+        <SparklesIcon />
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
